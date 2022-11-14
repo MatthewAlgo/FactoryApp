d1715a89ec9652b0fb83f42f8541aac420f61c5c
Your Name <you@example.com>
2022-11-14 22:27:30 +0200

    Changed login provider, solved bug on logout, laid the foundations for implementing onboarding screens

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,67 @@
+import 'package:factoryapp/home/top_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:floating_navigation_bar/floating_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+     // Create a fully featured scaffold
+    return Scaffold(
+      // Create a appbar
+      appBar: getAppBarForApp(context),
+      // Create a body
+      body: Center(
+        // Page view with 3 pages
+        child: PageView(
+          children: [
+            // First page
+            Center(
+              child: Text('Page 1'),
+            ),
+            // Second page
+            Center(
+              child: Text('Page 2'),
+            ),
+            // Third page
+            Center(
+              child: Text('Page 3'),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: FloatingNavigationBar(
+        barWidth: // Set the width of the bar
+            MediaQuery.of(context).size.width,
+        backgroundColor: Color.fromARGB(221, 113, 142, 237),
+        iconColor: Colors.white,
+        textStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 12.0,
+        ),
+        iconSize: 15.0,
+        items: [
+          NavBarItems(icon: EvaIcons.homeOutline, title: "Home"),
+          NavBarItems(icon: EvaIcons.shoppingCartOutline, title: "Cart"),
+          NavBarItems(icon: EvaIcons.activity, title: "Profile"),
+        ],
+        onChanged: (value) {
+          // USE YOUR STATE MANAGEMENT TECHNIQUE TO GET
+          // AND CHANGE INDEX OF NAVIGATION BAR
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,59 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:factoryapp/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:login_screen/controller/auth_controller.dart';
-import 'package:login_screen/screens/home_screen/home_screen.dart';
-import 'package:login_screen/screens/splash_screen/flutterlogo.dart';
-import 'package:login_screen/screens/splash_screen/splash_screen.dart';
+import 'package:factoryapp/login/Screens/Welcome/welcome_screen.dart';
+import 'package:factoryapp/login/constants.dart';
 
-import 'screens/login_screen/login_screen.dart';
-import 'utils/constants.dart';
+import 'package:get/get.dart';
+
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp().then((value) => Get.put(AuthController()));
+  await Firebase.initializeApp();
 
   runApp(const MyApp());
 }
 
+// TODO: Email verification
+// TODO: Shared preferences to add an onboarding screen on first app launch
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Factory',
       debugShowCheckedModeBanner: false,
+      title: 'FactoryApp',
       theme: ThemeData(
-        scaffoldBackgroundColor: kBackgroundColor,
-        textTheme: Theme.of(context).textTheme.apply(
-              bodyColor: kPrimaryColor,
-              fontFamily: 'Montserrat',
+          primaryColor: kPrimaryColor,
+          scaffoldBackgroundColor: Colors.white,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+              primary: kPrimaryColor,
+              shape: const StadiumBorder(),
+              maximumSize: const Size(double.infinity, 56),
+              minimumSize: const Size(double.infinity, 56),
             ),
-      ),
+          ),
+          inputDecorationTheme: const InputDecorationTheme(
+            filled: true,
+            fillColor: kPrimaryLightColor,
+            iconColor: kPrimaryColor,
+            prefixIconColor: kPrimaryColor,
+            contentPadding: EdgeInsets.symmetric(
+                horizontal: defaultPadding, vertical: defaultPadding),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+              borderSide: BorderSide.none,
+            ),
+          )),
       home: const Splash(),
-      routes: {
-        '/login': (context) => const LoginScreen(),
-        '/home': (context) => const HomeScreen(),
-        '/splash': (context) => const FlutterScreen(),
-      },
     );
   }
 }
